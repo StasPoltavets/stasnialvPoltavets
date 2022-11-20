de6d7bebb73ec433852eb4fedafed823089f1e16
StasPoltavets <34444369+StasPoltavets@users.noreply.github.com>
2022-11-20 11:16:12 +0200

    Calculator home work 7

---
+++ src/HomeWork7/CalculatorBasic.java
@@ -0,0 +1,63 @@
+package HomeWork7;
+
+import java.util.Scanner;
+
+public class CalculatorBasic {
+    Double num1, num2;
+    String operation;
+
+    public void setNum() {
+        System.out.println("Введіть число");
+        this.num1 = Double.valueOf(new Scanner(System.in).nextLine());
+        System.out.println("Введіть число");
+        this.num2 = Double.valueOf(new Scanner(System.in).nextLine());
+    }
+    public void setNum(int sqrt, int sin ) {
+        System.out.println("Введіть число");
+        this.num2 = Double.valueOf(new Scanner(System.in).nextLine());
+    }
+
+    public void setOperation() {
+        System.out.println("Введіть операцію");
+        this.operation = new Scanner(System.in).nextLine();
+    }
+
+    public void executeOperation() {
+        switch (operation) {
+            case "+":
+                getSumm();
+                break;
+            case "-":
+                getSubtraction();
+                break;
+            case "*":
+                getMultiplication();
+                break;
+            case "/":
+                getDivision();
+                break;
+        }
+
+    }
+
+    public void getSumm() {
+        System.out.println(num1 + num2);
+    }
+
+    public void getSubtraction() {
+        System.out.println(num1 - num2);
+    }
+
+    public void getMultiplication() {
+        System.out.println(num1 * num2);
+    }
+
+    public void getDivision() {System.out.println(num1 / num2);}
+
+    public void processWorkCalc() {
+            setNum();
+            setOperation();
+            executeOperation();
+    }
+
+}
---
+++ src/HomeWork7/CalculatorPro.java
@@ -0,0 +1,32 @@
+package HomeWork7;
+import java.util.Scanner;
+public class CalculatorPro extends CalculatorBasic {
+
+    @Override
+    public void executeOperation() {
+        switch (operation) {
+            case "sqrt":
+                setNum(1, 1);
+                getRoot();
+                break;
+            case "sin":
+                setNum(1, 1);
+                getSinx();
+                break;
+        }
+    }
+    public void getRoot() {
+        num1 = Math.sqrt(num2);
+        System.out.println("Корінь " + num1);
+    }
+    public void getSinx() {
+        num1 = Math.sin(num2);
+        System.out.println("Sin( " + num2 + " ) = " + num1);
+    }
+    @Override
+    public void processWorkCalc() {
+            System.out.println("Виберіть дію sin, sqrt");
+            setOperation();
+            executeOperation();
+    }
+}
