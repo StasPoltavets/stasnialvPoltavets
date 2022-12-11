e8b9eeea1347f0c1e215fd332efdda22b470eb21
StasPoltavets <34444369+StasPoltavets@users.noreply.github.com>
2022-12-11 16:28:41 +0200

    Some Exception варіант 2

--- src/HomeWorkException/NotFoundPlayers.java
+++ src/HomeWorkException/NotFoundPlayers.java
@@ -1,7 +1,10 @@
 package HomeWorkException;
 
+import static HomeWorkException.PokerException.players;
+
 public class NotFoundPlayers extends RuntimeException{
-    public NotFoundPlayers(int CheckPlayerCount) {
-        super("Недостатньо гравців" + CheckPlayerCount);
+    public NotFoundPlayers(String не_достатньо_гравців) {
+            System.out.println(ConsoleColors.CYAN_BACKGROUND +"Не достатньо гравців за столом");
+       // super(ConsoleColors.GREEN_UNDERLINED +"Не достатньо гравців за столом");
     }
 }
