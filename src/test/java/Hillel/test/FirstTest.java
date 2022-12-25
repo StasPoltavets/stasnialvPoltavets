f193f29b1e3ffc9fd1df4dfee29753fb85a333e3
StasPoltavets <34444369+StasPoltavets@users.noreply.github.com>
2022-12-25 12:17:57 +0200

    домашка по maven

--- src/test/java/Hillel/test/FirstTest.java
+++ src/test/java/Hillel/test/FirstTest.java
@@ -5,7 +5,7 @@ import org.testng.annotations.Test;
 public class FirstTest {
     @Test
     public void firstTest (){
-        System.out.println("test");
+        System.out.println("my first test");
         System.out.println("Running test on " + System.getProperty("url"));
 
     }
